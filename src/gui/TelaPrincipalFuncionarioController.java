fccee9bf58220909f50795195454cbc23a4fd479
Mateus Carriel <mateuscarriel2016@gmail.com>
2024-11-25 18:52:43 -0300

    Cliente registrado pelo funcionario

--- src/gui/TelaPrincipalFuncionarioController.java
+++ src/gui/TelaPrincipalFuncionarioController.java
@@ -2,8 +2,8 @@ package gui;
 
 import java.io.IOException;
 
-import gui.util.Utils;
 import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,7 +14,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import model.entities.Cliente;
 import model.entities.Veterinario;
+import model.services.ClienteService;
 import model.services.VeterinarioService;
 
 public class TelaPrincipalFuncionarioController {
@@ -68,10 +70,32 @@ public class TelaPrincipalFuncionarioController {
     // Método chamado para cadastrar um novo cliente
     @FXML
     public void onCadastrarCliente(ActionEvent event) {
-        System.out.println("Cadastro de novo Cliente.");
-        // Lógica para abrir a tela de cadastro de cliente
+        try {
+            // Carrega o arquivo FXML da tela de cadastro de cliente
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/ClienteRegistro.fxml"));
+            Parent parent = loader.load();
+
+            // Configura o controlador da nova tela
+            ClienteRegistroController controller = loader.getController();
+            controller.setClienteService(new ClienteService()); // Passando o serviço para o controlador
+            controller.setCliente(new Cliente()); // Inicializando a entidade no controlador
+
+            // Exibe a tela em uma nova janela
+            Stage stage = new Stage();
+            stage.setTitle("Cadastro de Cliente");
+            stage.setScene(new Scene(parent));
+            stage.initOwner(Utils.currentStage(event)); // Configura o stage pai
+            stage.initModality(Modality.WINDOW_MODAL); // Define a modalidade
+            stage.showAndWait(); // Espera a tela ser fechada antes de continuar
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Alerts.showAlert("Erro", "Erro ao carregar tela", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 
+   
+    
     // Método chamado para aceitar ou marcar uma consulta
     @FXML
     public void onMarcarConsulta(ActionEvent event) {
